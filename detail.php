b34d275bd5f943431cdfe78db9e3ef9d3440c481
parallel-kevin <601406483@qq.com>
2017-07-05 16:03:10 +0800

    1、详情页展示 2、修改目录结构

---
+++ detail.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * User: xukaiwen@baixing.com
+ */
+
+$id = $_GET['id'];
+$sql = 'SELECT * FROM `contacts` WHERE `id`='.$id.';';
+require('connectDatabase.php');
+
+$stmt = $db->query($sql);
+$row = $stmt->fetch_row();
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>详情</title>
+    <link rel="stylesheet" href="css/detail.css">
+</head>
+<body>
+<h1>
+    详情
+</h1>
+<ul>
+    <li>
+        姓名：
+        <span><?php echo $row[1]; ?></span>
+    </li>
+    <li>
+        邮件：
+        <span><?php echo $row[2]; ?></span>
+    </li>
+    <li>
+        电话：
+        <span><?php echo $row[3]; ?></span>
+    </li>
+    <li>
+        生日：
+        <span><?php echo $row[4]; ?></span>
+    </li>
+</ul>
+<div id="delete">
+    <form action="delete.php" method="post">
+        <input name="id" id="userId" value="<?php echo $id ?>" />
+        <input type="submit" value="删除" />
+    </form>
+</body>
+</html>
--- index.php
+++ index.php
@@ -2,7 +2,8 @@
 /**
 * User: xukaiwen@baixing.com
 */
-$sql = 'SELECT * FROM `contacts`';
+
+$sql = 'SELECT `id`, `name` FROM `contacts`';
 require('./connectDatabase.php');
 ?>
 
@@ -10,18 +11,18 @@ require('./connectDatabase.php');
 <html>
 <head>
     <title>通讯录</title>
-    <link rel="stylesheet" href="index.css">
+    <link rel="stylesheet" href="css/index.css">
 </head>
 <body>
     <table width="100%">
         <caption>通讯录</caption>
         <tbody>
-        <?php
-        $stmt = $db->query($sql);
-        while($row = $stmt->fetch_row()) {
-            echo "<tr><td><a href='detail.php?id=".$row[0]."'>".$row['1']."</a></td></tr>";
-        }
-        ?>
+            <?php
+            $stmt = $db->query($sql);
+            while($row = $stmt->fetch_row()) {
+                echo "<tr><td><a href='detail.php?id=".$row[0]."'>".$row['1']."</a></td></tr>";
+            }
+            ?>
         </tbody>
     </table>
 </body>
